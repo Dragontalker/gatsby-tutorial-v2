e6553109bf66b858db7e39b8a7e6056872a8ad15
Richard <richard.yang.tong@gmail.com>
2021-04-11 10:00:13 -0400

    Fixed formatting.

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function error() {
     return (
         <div>
-            <h1>this is our error page
-            </h1>
+            <h1>this is our error page</h1>
         </div>
     )
 }
